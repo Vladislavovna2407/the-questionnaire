debbb40cf982befb85cca71e0869f929da8dbb91
Vladislavovna2407 <116091953+Vladislavovna2407@users.noreply.github.com>
2023-01-07 23:45:26 +0000

    hometask

--- script.js
+++ script.js
@@ -4,5 +4,52 @@ form.addEventListener("submit", (event) => {
   // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
 
-  // Здесь твой код
+  fetch('http://46.21.248.81:3001/user', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer: Vladislavovna2407'
+        },
+        body: JSON.stringify({
+            name: 'Катя',
+            secondName: 'Сергеева',
+            phone: 89776000000,
+            email: 'Katya@gmail.com',
+            agree: true
+        }),
+    })
+
+    .then((result) => {
+            let forma = document.querySelector('.form');
+            forma.reset();
+            return result.json();
+        })
+        .then(data => alert("Данные отправлены"))
+
+
+});
+
+
+nameValue = document.querySelector('#name');
+secondNameValue = document.querySelector('#secondName');
+phoneValue = document.querySelector('#phone');
+emailValue = document.querySelector('#email');
+agreeValue = document.querySelector('#agree');
+
+
+fetch('http://46.21.248.81:3001/my-users', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer: Vladislavovna2407'
+        },
+
+    })
+    .then((answer) => {
+        return answer.json();
+    })
+    .then((value) => {
+        console.log(value)
+    });
 });
