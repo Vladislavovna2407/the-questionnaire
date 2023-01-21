b07c9f2d86322205f1c52b1c84862ebdf3c0a763
Vladislavovna2407 <Vladislavovna2018@bk.ru>
2023-01-21 16:28:37 +0100

    Домашнее задание анкета

--- script.js
+++ script.js
@@ -3,6 +3,51 @@ form.addEventListener("submit", (event) => {
   // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
   // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
+  event.preventDefault();
+  fetch('http://46.21.248.81:3001/user', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer: Vladislavovna2407'
+      },
+      body: JSON.stringify({
+          name: nameValue.value,
+          secondName: secondNameValue.value,
+          phone: phoneValue.value,
+          email: emailValue.value,
+          agree: agreeValue.value
+      }),
+  })
 
-  // Здесь твой код
+  .then((result) => {
+          let forma = document.querySelector('.form');
+          forma.reset();
+          return result.json();
+      })
+      .then(data => alert("Данные отправлены"))
+  
 });
+
+
+let nameValue = document.querySelector('#name');
+let secondNameValue = document.querySelector('#secondName');
+let phoneValue = document.querySelector('#phone');
+let emailValue = document.querySelector('#email');
+let agreeValue = document.querySelector('#agree');
+
+
+fetch('http://46.21.248.81:3001/my-users', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer: Vladislavovna2407'
+        },
+
+    })
+    .then((answer) => {
+        return answer.json();
+    })
+    .then((value) => {
+        console.log(value)
+    });
