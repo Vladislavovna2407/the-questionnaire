842ab61bbc880bc63854a506718b407b0e44bb34
Vladislavovna2407 <Vladislavovna2018@bk.ru>
2023-02-01 16:08:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", (event) => {
   // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
   // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
-  fetch('https://polinashneider.space/user', {
+  fetch('https://polinashneider.space/', {
       method: 'POST',
       headers: {
           'Accept': 'application/json',
